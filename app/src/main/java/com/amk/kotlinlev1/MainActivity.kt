443493a594a8e67bf54f850fde93574944f8797a
Mikhail <MIKHAILVALKOPYLOV@YANDEX.Ru>
2020-11-16 14:23:14 +0300

    Revert "home work 1"
    This reverts commit b1a2ffe394f17788308bcf56ca848acca83572ea.

--- app/src/main/java/com/amk/kotlinlev1/MainActivity.kt
+++ app/src/main/java/com/amk/kotlinlev1/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.amk.kotlinlev1
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import com.amk.kotlinlev1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val modelView = ViewModelProvider(this).get(MainModelView::class.java)
-        modelView.observeLiveDate().observe(this) { data: String ->
-            textView.text = data
-        }
-
-        button.setOnClickListener {
-            modelView.buttonClicked()
-        }
     }
 }
